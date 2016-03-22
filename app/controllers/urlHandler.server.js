ca7f62693b635501aff73cf7a284112e32a815cc
Alex Evans <alex.evans.d@gmail.com>
2016-03-22 01:07:10 +0000

    Error with Node.js, looking for help

---
+++ app/controllers/urlHandler.server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function urlHandler(db) {
+/*    
+    var urls = db.collection('urls');
+    
+    this.getShortUrl = function(req, res) {
+        urls.findOne({}, {sort:{$natural:-1}}, function(err, test) {
+            if(err) {throw err;}
+            
+            res.json({"url": "test"});
+            db.close();
+        });
+        
+    };
+    
+    this.getRedirectUrl = function(req, res) {
+        res.json("test");
+        db.close();
+    };
+*/    
+}
+
+module.exports = urlHandler;
