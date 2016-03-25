19c77266a8159ac262323404bfe7730d749de999
Alex Evans <alex.evans.d@gmail.com>
2016-03-25 02:22:26 +0000

    Attempt to use local urlMatch and lastSeq instead of Async calls, looks like might be easier to use Async

--- app/controllers/urlHandler.server.js
+++ app/controllers/urlHandler.server.js
@@ -6,23 +6,34 @@ function urlHandler(db) {
     
     this.getShortUrl = function(req, res) {
 
+        var orgUrl = req.url.substring(5);
+        var idSeq = 0;
+
+        // if orginal URL exists already in db return that seq otherwise add a new entry
+        var urlMatch = urls.findOne({"url": orgUrl});
+        var lastSeq = urls.findOne({}, {sort:{$natural:-1}});
+        
+        if(typeof urlMatch._id == "number") {
+            idSeq = urlMatch._id;
+        } else if(typeof lastSeq._id == "number") {
+            idSeq = lastSeq + 1;
+        }
+        
+        var shrtUrl = "https://url-shortener-microservice-alcatrats.c9users.io/" + idSeq;
+        
+        urls.insert(
+            {
+                _id: idSeq,
+                url: orgUrl
+            }
+        );
+
+        res.json({"original_url": orgUrl, "short_url": shrtUrl, "match": urlMatch, "last": lastSeq._id});
+        
+/*        
         urls.findOne({}, {sort:{$natural:-1}}, function(err, urlFnd) {
             if(err) {throw err;}
 
-            
-/*
-            urls.insert({"url": "test"}, function(err) {
-                if(err) {throw err;}
-                
-                urls.findOne({},{sort:{$natural:-1}}, function(err, data) {
-                    if(err) {throw err;}
-                    
-                    res.json(data);
-                });
-                    
-            });
-*/      
-            var orgUrl = req.url.substring(5);
             var idSeq = 0;
             if(typeof urlFnd._id == "number") {
                 idSeq = urlFnd._id + 1;
@@ -35,10 +46,9 @@ function urlHandler(db) {
                     url: orgUrl
                 }
             );
-            
-            res.json({"original_url": orgUrl, "short_url": shrtUrl, "urlFnd": urlFnd});
-            
+
         });
+*/
 
     };
     
